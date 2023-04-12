7968064947d3cdde63d1ca5617b02e0fe7cc54c4
joan <joan.espar@gmail.com>
2023-04-12 15:11:03 +0200

    hardhat setup

---
+++ contracts/PokerFinance.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ER20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PokerFinance {}
